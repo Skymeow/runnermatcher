ad553d165ea6ef38ead16b538768e633f534974f
Sky Xu <skyrocks88xmz@gmail.com>
2017-08-15 12:43:14 -0400

    Gah finally fixed the matching bug

--- iosapp/iosapp/ViewController.swift
+++ iosapp/iosapp/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var long: Double?
     var lat: Double?
     var userCoordinate : YLPCoordinate?
-
+    
     func locationManager(_ manage: CLLocationManager,didUpdateLocations locations: [CLLocation]) {
         let location = locations.last
         self.long = Double((location?.coordinate.longitude)!)
@@ -54,7 +54,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let locationRef = Database.database().reference().child("location").child(currentUser.uid)
         locationRef.updateChildValues(dict)
         
-    CLGeocoder().reverseGeocodeLocation((manager.location)!, completionHandler: {(CLPlacemark, error) -> Void in
+        CLGeocoder().reverseGeocodeLocation((manager.location)!, completionHandler: {(CLPlacemark, error) -> Void in
             if error != nil {
                 print("Reverse geocoder failed with error: \(String(describing: error?.localizedDescription))")
                 return
@@ -62,8 +62,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             if (CLPlacemark?.count)!>0{
                 let placemark = CLPlacemark?.last!
                 self.postalCode = placemark!.postalCode
-//                let zipRef = Database.database().reference().child("zipcode").child(self.postalCode!).child("userUID")
-//                zipRef.setValue(currentUser.uid)
+                //                let zipRef = Database.database().reference().child("zipcode").child(self.postalCode!).child("userUID")
+                //                zipRef.setValue(currentUser.uid)
                 let code = String(describing: self.postalCode!)
                 
                 let first2 = code.substring(to:code.index(code.startIndex, offsetBy: 2))
@@ -84,31 +84,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
         })
         
-        }
-//    end of location manager
-func update(){
-    if (self.keyArr?.isEmpty) == true {
+    }
+    //    end of location manager
+    func update(){
+        if (self.keyArr?.isEmpty) == true {
             let fakeURL = URL(string: "https://img.usmagazine.com/social/katy-perry-taylor-swift-08322c54-b352-4a33-8d81-a57a3dc1b366.jpg")
             self.runnerImage.contentMode = .scaleAspectFit
             self.runnerImage.kf.setImage(with: fakeURL)
             print("Yo we done")
-    }else{
-        let randomIndex = Int(arc4random_uniform(UInt32((self.keyArr?.count)!)))
-        let randomUID = self.keyArr?[randomIndex]
-        self.displayedUID = randomUID!
-        let queryRef = Database.database().reference().child("users").child(randomUID!)
-        queryRef.observeSingleEvent(of: .value, with: {(snapshot) in
-            let value = snapshot.value as? [String : Any]
-            let imageString = value?["profile_pic"] as? String ?? ""
-            print(imageString)
-            let profileImage = URL(string: imageString)
-            self.runnerImage.contentMode = .scaleAspectFit
-            self.runnerImage.kf.setImage(with: profileImage)
-        })
-    }
+        }else{
+            let randomIndex = Int(arc4random_uniform(UInt32((self.keyArr?.count)!)))
+            let randomUID = self.keyArr?[randomIndex]
+            //        let pendingRef = Database.database().reference().child("swiped").child(User.current.uid)
+            //        pendingRef.child("pending").updateChildValues(["\(String(describing: randomUID!))" : true])
+            self.displayedUID = randomUID!
+            let checkRef = Database.database().reference().child("swiped").child(self.displayedUID).child("accepted")
 
+            checkRef.observeSingleEvent(of: .value, with: {(snapshot) in
+                if snapshot.hasChild(User.current.uid) {
+                    let matcherRef = Database.database().reference().child("matchers")
+                    let matchingRef = Database.database().reference().child("matching")
+                    matcherRef.child(self.displayedUID).updateChildValues(["\(User.current.uid)":false])
+                    matchingRef.child(User.current.uid).updateChildValues(["\(self.displayedUID)":false])
+                } else {
+                    print("no one likes you")
+                }
+            })
+            let queryRef = Database.database().reference().child("users").child(randomUID!)
+            queryRef.observeSingleEvent(of: .value, with: {(snapshot) in
+                let value = snapshot.value as? [String : Any]
+                let imageString = value?["profile_pic"] as? String ?? ""
+                print(imageString)
+                let profileImage = URL(string: imageString)
+                self.runnerImage.contentMode = .scaleAspectFit
+                self.runnerImage.kf.setImage(with: profileImage)
+            })
+        }
+        
     }
-
+    
     func wasDragged(gestureRecognizer: UIPanGestureRecognizer) {
         let currentUser = User.current
         let translation = gestureRecognizer.translation(in: view)
@@ -130,7 +144,6 @@ func update(){
         
         
         if gestureRecognizer.state == UIGestureRecognizerState.ended {
-            
             var acceptedOrRejected = ""
             var ignored =  [""]
             let swipedRef = Database.database().reference().child("swiped").child(currentUser.uid)
@@ -145,32 +158,37 @@ func update(){
                 acceptedOrRejected = "accepted"
                 swipedRef.child("accepted").updateChildValues(["\(displayedUID)" : true])
                 ignored.append("\(displayedUID)")
+                let matcherRef = Database.database().reference().child("matchers")
+                let matchingRef = Database.database().reference().child("matching")
+                matcherRef.child(displayedUID).updateChildValues(["\(User.current.uid)":true])
+                matchingRef.child(User.current.uid).updateChildValues(["\(displayedUID)":true])
+
             }
             if acceptedOrRejected != "" && displayedUID != ""{
-//                 keyArr = keyArr?.filter{$0 != "\(displayedUID)"}
+                //                 keyArr = keyArr?.filter{$0 != "\(displayedUID)"}
                 keyArr = Array(Set(keyArr!).subtracting(ignored))
                 update()
             }
-
+            
             rotation = CGAffineTransform(rotationAngle: 0)
             stretchAndRotation = rotation.scaledBy(x: 1, y: 1) // rotation.scaleBy(x: scale, y: scale) is now rotation.scaledBy(x: scale, y: scale)
             label.transform = stretchAndRotation
             
             label.center = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
-        
+            
         }
         
     }
-//end wasDragged function
-
-//barbuttontapped is show pop out now
+    //end wasDragged function
+    
+    //barbuttontapped is show pop out now
     @IBAction func BarButtonTapped(_ sender: UIButton) {
         popupConstraint.constant = 0
         UIView.animate(withDuration: 0.2, animations: {self.view.layoutIfNeeded()
         })
         let fakeCoordinate = YLPCoordinate(latitude: 23.293, longitude: -123.233)
-//        userCoordinate = YLPCoordinate(latitude: lat!, longitude: long!)
-//        print(userCoordinate!)
+        //        userCoordinate = YLPCoordinate(latitude: lat!, longitude: long!)
+        //        print(userCoordinate!)
         let query = YLPQuery(coordinate: fakeCoordinate)
         query.term = "bar"
         query.limit = 3
@@ -181,11 +199,11 @@ func update(){
                 if let topBusiness = search.businesses.first {
                     print("Top business: \(topBusiness.name)+\(topBusiness.categories.description.description)")
                     print(topBusiness.location.address)
-//                    print(topBusiness.categories)
+                    //                    print(topBusiness.categories)
                 } else {
                     print("No businesses found")
                 }
-        
+                
             }.onFailure { error in
                 print("Search errored: \(error)")
                 exit(EXIT_FAILURE)
@@ -206,57 +224,57 @@ func update(){
             viewWithTag.removeFromSuperview()
             
         }else{
-        print("No!")
+            print("No!")
         }
     }
     
-  override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
-    let videoPath = Bundle.main.path(forResource: "run", ofType:"mp4")
-    let imagePath = Bundle.main.path(forResource: "runner", ofType: "jpeg")!
-    let options = VideoOptions(pathToVideo: videoPath!, pathToImage: imagePath, isMuted: true, shouldLoop: true)
-    let videoView = VideoBackground(frame: view.frame, options: options)
-    videoView.tag = 100
-    videoView.isUserInteractionEnabled = true
-    view.addSubview(videoView)
-    
-    let aSelector : Selector = #selector(ViewController.removeSubview)
-    let tapGesture = UITapGestureRecognizer(target:self, action: aSelector)
-    videoView.addGestureRecognizer(tapGesture)
-    
-    let textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 30))
-    textLabel.center = CGPoint(x: 160, y: 285)
-    textLabel.textAlignment = .center
-    textLabel.isUserInteractionEnabled = true
-    self.view.addSubview(textLabel)
-    textLabel.fadeIn(completion: {
-        (finished: Bool) -> Void in
-         textLabel.text = "Tap screen to get started!"
-    })
-    
-    textLabel.fadeOut()
-
-//for location manager
+        let videoPath = Bundle.main.path(forResource: "run", ofType:"mp4")
+        let imagePath = Bundle.main.path(forResource: "runner", ofType: "jpeg")!
+        let options = VideoOptions(pathToVideo: videoPath!, pathToImage: imagePath, isMuted: true, shouldLoop: true)
+        let videoView = VideoBackground(frame: view.frame, options: options)
+        videoView.tag = 100
+        videoView.isUserInteractionEnabled = true
+        view.addSubview(videoView)
+        
+        let aSelector : Selector = #selector(ViewController.removeSubview)
+        let tapGesture = UITapGestureRecognizer(target:self, action: aSelector)
+        videoView.addGestureRecognizer(tapGesture)
+        
+        let textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 30))
+        textLabel.center = CGPoint(x: 160, y: 285)
+        textLabel.textAlignment = .center
+        textLabel.isUserInteractionEnabled = true
+        self.view.addSubview(textLabel)
+        textLabel.fadeIn(completion: {
+            (finished: Bool) -> Void in
+            textLabel.text = "Tap screen to get started!"
+        })
+        
+        textLabel.fadeOut()
+        
+        //for location manager
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
-//    handle swiping
+        //    handle swiping
         let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.wasDragged(gestureRecognizer:)))
         runnerImage.isUserInteractionEnabled = true
         runnerImage.addGestureRecognizer(gesture)
-    
-    //for popup
-    popupView.layer.cornerRadius = 10
-    popupView.layer.masksToBounds = true
+        
+        //for popup
+        popupView.layer.cornerRadius = 10
+        popupView.layer.masksToBounds = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
-
-
+    
+    
 }
 
 extension UIView {
